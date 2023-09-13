4be246eecf1ccf9348e89eac573c9b90d1514c98
WebRizen <webrizen@gmail.com>
2023-09-13 09:06:17 +0530

    man

--- src/App.jsx
+++ src/App.jsx
@@ -10,15 +10,15 @@ function App() {
             <h1 class="title-font sm:text-4xl text-5xl mb-2 font-bold text-gray-100">
               Globiance Incubator
             </h1>
-            <p class="mb-4 leading-relaxed text-lg">
+            <p class="sm:mb-4 mb-2 leading-relaxed text-lg">
               A solution for something important with the incubator you can
               incubate all over the place.
             </p>
             <div class="flex justify-center w-min gap-2">
-            <button className="inline-flex items-center border-2 border-gray-200 bg-white hover:bg-transparent hover:text-white py-2 px-8 hover:outline-none focus:outline-none hover:bg-white text-gray-900 rounded text-base md:mr-4 mt-4 md:mt-0 w-min whitespace-nowrap">
+            <button className="inline-flex items-center border-2 border-gray-200 bg-white hover:bg-transparent hover:text-white py-2 px-8 hover:outline-none focus:outline-none hover:bg-white text-gray-900 rounded sm:text-base text-xs md:mr-4 mt-4 md:mt-0 w-min whitespace-nowrap">
                   Incubate Now
                 </button>
-                <button className="inline-flex items-center border-2 border-gray-200 bg-transparent py-2 px-8 hover:outline-none focus:outline-none hover:bg-white hover:text-gray-900 rounded text-base mt-4 md:mt-0 w-min whitespace-nowrap">
+                <button className="inline-flex items-center border-2 border-gray-200 bg-transparent py-2 px-8 hover:outline-none focus:outline-none hover:bg-white hover:text-gray-900 rounded sm:text-base text-xs mt-4 md:mt-0 w-min whitespace-nowrap">
                   Incubate Later
                 </button>
             </div>
